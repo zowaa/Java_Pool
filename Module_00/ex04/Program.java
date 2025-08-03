f349c9618d830398224902d91435ad413deaa783
zowaa <elfilfatimaezzahra0@gmail.com>
2025-08-03 15:25:18 +0100

    ex04 done

--- Module_00/ex04/Program.java
+++ Module_00/ex04/Program.java
@@ -8,20 +8,57 @@ public class Program {
         char[] line_t = line.toCharArray();
         char[] char_T = new char[line.length()];
         int[] occurr_count = new int[line.length()];
-        int size;
+        int[] heights = new int[10];
+        int size, chart_col;
 
         size = count_occurrences(line_t, char_T, occurr_count);
+        chart_col = size < 10 ? size : 10;
         sort_occurrences(size, char_T, occurr_count);
+        calculate_heights(chart_col, heights, occurr_count);
 
+        char[][] chart = new char[12][chart_col * 3];
+        fill_chart(chart, char_T, occurr_count, heights, chart_col);
 
-//        for (int i = 0; i < size; i++){
-//            System.out.print(char_T[i] + "  ");
-//        }
-//        System.out.println("");
-//        for (int i = 0; i < size; i++){
-//            System.out.print(occurr_count[i] + " ");
-//        }
-//        System.out.println("");
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < chart_col * 3; j++) {
+                System.out.print(chart[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void fill_chart(char[][] chart, char[] char_T, int[] occurr_count, int[] heights, int chart_col) {
+        int k = 0;
+        for (int j = 2; j < chart_col * 3; j += 3) {
+            int i = 11;
+            chart[i][j - 2] = ' ';
+            chart[i][j - 1] = ' ';
+            chart[i--][j] = char_T[k];
+            while (heights[k] != 0) {
+                chart[i][j - 2] = ' ';
+                chart[i][j - 1] = ' ';
+                chart[i--][j] = '#';
+                heights[k]--;
+            }
+            int h = j;
+            int l = 3;
+            while (occurr_count[k] != 0) {
+                chart[i][h--] = (char) (occurr_count[k] % 10 + '0');
+                occurr_count[k] /= 10;
+                l--;
+            }
+            while(l > 0) {
+                chart[i][h--] = ' ';
+                l--;
+            }
+            k++;
+        }
+    }
+
+    static void calculate_heights(int chart_col, int[] heights, int[] occurr_count) {
+        for (int i = 0; i < chart_col; i++) {
+            heights[i] = (occurr_count[i] * 10) / occurr_count[0];
+        }
     }
 
     static void sort_occurrences(int size, char[] char_T, int[] occurr_count) {
